d02a3e903fde568331a170905fb31be1f075a40d
batingacrisjohn <168161751+batingacrisjohn@users.noreply.github.com>
2024-04-26 15:55:51 +0800

    Update number-challenge.js

--- number-challenge.js
+++ number-challenge.js
@@ -4,3 +4,23 @@
     3 Create a function that will return the product of the 2 numbers
     4 Create a function that will return the average of the 2 numbers
 */
+Function sum(num1, num2, num3) {
+    Return num1 + num2 + num3;
+}
+
+Function difference(num1, num2) {
+    Return num1 – num2;
+}
+
+Function product(num1, num2) {
+    Return num1 * num2;
+}
+
+Function average(num1, num2) {
+    Return (num1 + num2) / 2;
+}
+
+Console.log(“Sum:”, sum(3, 5, 7));
+Console.log(“Difference:”, difference(10, 5));
+Console.log(“Product:”, product(4, 6));
+Console.log(“Average:”, average(10, 20));
